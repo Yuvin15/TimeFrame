739d7f1c0cda94ca73a81bb65d5c9b2604b62f60
unknown <yuvinpadayachee@gmail.com>
2025-06-01 19:51:03 +0200

    Added door to open when throwing object

---
+++ Assets/Assets/OpenDoor.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    [Header("Door Settings")]
+    public bool openDoor =  false;
+    public bool closeDoor =  true;
+    public int timerToClose =  2;
+
+    private void OnTriggerEnter(Collider objectThrow)
+    {
+        if(objectThrow.CompareTag("ThrowObjects")) 
+        { 
+            if(closeDoor == true) 
+            {
+                Debug.Log("Open Door");
+                transform.position = new Vector3(0, 10, 14);
+
+                CloseDoorAuto(timerToClose);
+            }
+        }
+    }
+
+    private void CloseDoorAuto(int time)
+    {
+        closeDoor = true;
+        transform.position = new Vector3(0, 4, 14);
+    }
+}
---
+++ Assets/Assets/RespawnObject.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class RespawnObject : MonoBehaviour
+{
+    [Header("Height for object")]
+    public Vector3 respawnPosition = new Vector3(0, 1, 0);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -10f) 
+        {
+            Debug.Log("Fell to -10");
+            transform.position = respawnPosition;
+        }
+        else 
+        {
+            Debug.Log("No problem");
+        }
+    }
+}
